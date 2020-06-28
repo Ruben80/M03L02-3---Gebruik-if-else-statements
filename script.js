07e4252add5f9a1bbb793150ca28b9d95a4b0ffd
Ruben80 <43403734+Ruben80@users.noreply.github.com>
2020-06-28 15:52:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const age = 18;
+const isFemale = false;
+const driverStatus = "sjaak"
+
+if (age >= 18){
+    console.log("Je mag naar binnen")
+}
+
+else {
+    console.log("Helaas je bent nog geen 18")
+}
+
+if (isFemale === true){
+    console.log("Je mag naar binnen")
+}
+
+else {
+    console.log("Helaas je bent geen vrouw!")
+}
+
+if (driverStatus === "bob") {
+    console.log("Je mag naar huis rijden!")
+}
+
+else {
+    console.log("Het is te gevaarlijk om terug te rijden!")
+}
